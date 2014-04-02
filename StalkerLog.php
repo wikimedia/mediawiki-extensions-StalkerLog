7d4f336f364cc8a360393f2f02ed58eb797b6a3d
Siebrand Mazeland <siebrand@kitano.nl>
2014-04-02 19:10:40 +0200

    Migrate to JSON i18n
    Procedure per https://www.mediawiki.org/wiki/Manual:GenerateJsonI18n.php
    with shim.
    
    Change-Id: Ibf089aac18f570da87a24b6c1175ccdbc3c423b7

--- StalkerLog.php
+++ StalkerLog.php
@@ -2,7 +2,7 @@
 /**
  * Extension:StalkerLog - Log everytime someone logs in or logs out,
  * great for tracking productivity in offices (or stalking).
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -11,18 +11,19 @@
  * @author Chad Horohoe <innocentkiller@gmail.com>
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
- 
+
 $wgExtensionCredits['specialpage'][] = array(
 	'path'           => __FILE__,
 	'name'           => 'StalkerLog',
-	'version'        => '0.6',
+	'version'        => '0.7.0',
 	'url'            => 'http://mediawiki.org/wiki/Extension:StalkerLog',
 	'author'         => '[mailto:innocentkiller@gmail.com Chad Horohoe]',
 	'descriptionmsg' => 'stalkerlog-desc',
 );
 
 # Basic setup
-$wgExtensionMessagesFiles['stalkerlog'] = dirname(__FILE__) . '/' . 'StalkerLog.i18n.php';
+$wgMessagesDirs['StalkerLog'] = __DIR__ . '/i18n';
+$wgExtensionMessagesFiles['StalkerLog'] = dirname(__FILE__) . '/' . 'StalkerLog.i18n.php';
 $wgAdditionalRights[] = 'stalkerlog-view-log';
 $wgGroupPermissions['*']['stalkerlog-view-log'] = true;
 $wgHooks['UserLoginComplete'][] = 'wfStalkerLogin';
@@ -38,7 +39,7 @@ $wgLogActions['stalkerlog/logout'] = 'stalkerlog-log-logout';
 
 # Login hook function
 function wfStalkerLogin( &$user ) {
-	
+
 	$log = new LogPage( 'stalkerlog', false);
 	$log->addEntry( 'login', $user->getUserPage(), '', null, $user );
 	return true;
@@ -46,7 +47,7 @@ function wfStalkerLogin( &$user ) {
 
 # Logout hook function
 function wfStalkerLogout( &$user, &$inject_html, $old_name ) {
-	
+
 	$log = new LogPage( 'stalkerlog', false);
 	$log->addEntry( 'logout', Title::newFromText( NS_USER, $old_name ), '', null, User::newFromName( $old_name ) );
 	return true;
